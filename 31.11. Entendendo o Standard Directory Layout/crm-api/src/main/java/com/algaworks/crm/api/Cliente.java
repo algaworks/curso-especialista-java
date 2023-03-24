ecab79b914d006cb1d89dc77037524a08fda01d2
Thiago Faria <thiagofa@gmail.com>
2023-03-24 19:06:29 -0300

    Código-fonte das aulas

---
+++ 31.11. Entendendo o Standard Directory Layout/crm-api/src/main/java/com/algaworks/crm/api/Cliente.java
@@ -0,0 +1,29 @@
+package com.algaworks.crm.api;
+
+public class Cliente {
+
+    private String nome;
+    private int idade;
+
+    public Cliente(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
---
+++ 31.14. Instalando e adicionando dependências com Maven/crm-app/src/main/java/com/algaworks/crm/app/Principal.java
@@ -0,0 +1,25 @@
+package com.algaworks.crm.app;
+
+import com.algaworks.crm.api.CadastroCliente;
+import com.algaworks.crm.api.Cliente;
+
+import java.util.List;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        var cadastroCliente = new CadastroCliente();
+        cadastroCliente.adicionar(new Cliente("João da Silva", 30));
+        cadastroCliente.adicionar(new Cliente("Maria Conceição", 90));
+        cadastroCliente.adicionar(new Cliente("Sebastião das Couves", 50));
+        cadastroCliente.adicionar(new Cliente("Joaquina de Jesus", 45));
+        cadastroCliente.adicionar(new Cliente("Josefina Amaral", 25));
+
+        List<Cliente> clientes = cadastroCliente.consultar(cliente -> cliente.getIdade() > 40);
+
+        for (Cliente cliente : clientes) {
+            System.out.printf("%s - %d%n", cliente.getNome(), cliente.getIdade());
+        }
+    }
+
+}
